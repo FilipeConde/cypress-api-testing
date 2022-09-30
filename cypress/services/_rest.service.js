4e140d0e0d26b553544cd8e69eb373457bdf17ac
Filipe Conde <filipecondep@gmail.com>
2022-09-29 22:28:29 -0300

    create service _rest with http methods

---
+++ cypress/services/_rest.service.js
@@ -0,0 +1,40 @@
+export default class Rest {
+
+	static get(endpoint) {
+		return cy.request({
+			method: 'GET',
+			url: endpoint,
+			failOnStatusCode: false
+		})
+	}
+
+	static post(endpoint, body, headers = null) {
+		return cy.request({
+			method: 'POST',
+			url: endpoint,
+			body: body,
+			headers: headers,
+			failOnStatusCode: false
+		})
+	}
+
+	static put(endpoint, body, headers = null) {
+		return cy.request({
+			method: 'PUT',
+			url: endpoint,
+			body: body,
+			headers: headers,
+			failOnStatusCode: false
+		});
+	}
+	
+	static delete(endpoint, body = null, headers = null) {
+		return cy.request({
+			method: 'DELETE',
+			url: endpoint,
+			body: body,
+			headers: headers,
+			failOnStatusCode: false
+		});
+	}
+}
